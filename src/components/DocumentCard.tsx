d12a8f47916ae6e5effcc0b4735359fd96bb249f
Efe Eroğlu <210541019@firat.edu.tr>
2025-04-20 00:29:18 +0300

    Documentation page grid structure changed

--- src/components/DocumentCard.tsx
+++ src/components/DocumentCard.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import './DocumentCard.css';
+import { FiArrowRight } from 'react-icons/fi';
 
 interface DocumentCardProps {
   title: string;
   description: string;
-  icon: string;
+  icon: React.ReactNode;
   link: string;
+  accentColor?: string;
 }
 
-const DocumentCard: React.FC<DocumentCardProps> = ({ title, description, icon, link }) => {
+const DocumentCard: React.FC<DocumentCardProps> = ({ 
+  title, 
+  description, 
+  icon, 
+  link,
+  accentColor = '#4361ee'
+}) => {
   return (
-    <a href={link} className="document-card">
-      <div className="document-icon">{icon}</div>
-      <div className="document-content">
-        <h3 className="document-title">{title}</h3>
-        <p className="document-description">{description}</p>
+    <a 
+      href={link} 
+      className="doc-card"
+      style={{ '--accent-color': accentColor } as React.CSSProperties}
+    >
+      <div className="doc-card-inner">
+        <div className="doc-icon-container">
+          <div className="doc-icon">{icon}</div>
+        </div>
+        
+        <div className="doc-content">
+          <h3 className="doc-title">{title}</h3>
+          <p className="doc-description">{description}</p>
+        </div>
+        
+        <div className="doc-arrow">
+          <FiArrowRight className="arrow-icon" />
+        </div>
+        
+        <div className="doc-hover-effect"></div>
       </div>
     </a>
   );
