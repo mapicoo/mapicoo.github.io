1c3576c55bc1676665091d87b0df14c72c9dc4d6
Efe Eroğlu <210541019@firat.edu.tr>
2025-04-20 12:20:38 +0300

    Fixed redirects in footer and added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer';
-import HomePage from './pages/Home/HomePage';
-import AboutPage from './pages/About/AboutPage';
-import TeamPage from './pages/Team/TeamPage';
-import DocumentationPage from './pages/Documentation/DocumentationPage';
-import ContactPage from './pages/Contact/ContactPages';
-import './App.css';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/Home/HomePage";
+import AboutPage from "./pages/About/AboutPage";
+import TeamPage from "./pages/Team/TeamPage";
+import DocumentationPage from "./pages/Documentation/DocumentationPage";
+import ContactPage from "./pages/Contact/ContactPages";
+import NotFoundPage from "./pages/Error/NotFound";
+import "./App.css";
 
 const App: React.FC = () => {
+  const location = useLocation();
+  const is404Page = location.pathname === "*";
+
   return (
     <div className="app">
       <Header />
@@ -20,6 +24,7 @@ const App: React.FC = () => {
           <Route path="/team" element={<TeamPage />} />
           <Route path="/documentation" element={<DocumentationPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
       <Footer />
